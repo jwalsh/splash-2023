0ff0a8edd34ee0f26106c192198f0745c1d7ee7e
Jason Walsh <j@wal.sh>
2023-10-23 15:26:49 +0100

    Summarize papers

---
+++ summarize-clj.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+# Path: summarize-clj.sh
+DELIMITER="--------------------------------------------------"
+# Prepare all of the .clj files to be sent in a ChatGPT or Claude chat for review: 
+for CLJ in $(find . -type f -name "*.clj"); do 
+    echo $DELIMITER >> linting.txt
+    echo "Linting $CLJ" >> linting.txt
+    clj-kondo --lint $CLJ >> linting.txt
+    echo "" >> linting.txt
+    echo "```" >> linting.txt
+    cat $CLJ >> linting.txt
+    echo "```" >> linting.txt
+    echo "" >> linting.txt
+done
